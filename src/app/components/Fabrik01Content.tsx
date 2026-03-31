ed3d1f065e1eba17c16fdd7f6ad78a714d1e19d8
Raphaël <market01@dataconforme.fr>
2026-03-31 10:35:28 +0200

    style: pull hero image left with negative translation in Fabrik01

--- src/app/components/Fabrik01Content.tsx
+++ src/app/components/Fabrik01Content.tsx
@@ -46,7 +46,7 @@ export default function Fabrik01Content({ onNavigate }: { onNavigate: (page: str
               Solutions françaises et souveraines
             </p>
           </div>
-          <div className="flex-1 relative">
+          <div className="flex-1 relative -translate-x-12 lg:-translate-x-24">
             <img 
               src="https://www.fabrik01.fr/images/ImageUne.png" 
               alt="Pilotage IA" 
